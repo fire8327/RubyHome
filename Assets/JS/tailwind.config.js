6d9c496d6cce483eef9edc7ff0d3e0fac5e3da38
fire8327 <114702513+fire8327@users.noreply.github.com>
2022-12-12 13:35:44 +0000

    create form

--- Assets/JS/tailwind.config.js
+++ Assets/JS/tailwind.config.js
@@ -3,16 +3,9 @@ tailwind.config = {
     extend: {
       colors: {            
           'text': '#030A1B',
-          'bgp':'#F9F9F9'
+          'bgp':'#F9F9F9',
+          'form':'#433E89'
         },
     },
-    screens: {
-      'ml':'425px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    }
   },
 };

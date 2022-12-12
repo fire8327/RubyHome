d17b5455545049b7fa71f492ba7a9cafa7f0a615
fire8327 <fire83274@gmail.com>
2022-12-12 21:29:12 +0300

    create swiper

---
+++ Assets/JS/swiper.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 640px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        }
+      }
+});

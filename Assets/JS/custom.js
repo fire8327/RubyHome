c3bb5efc545c7e6012a30a62359c61e8bfb68e9d
fire8327 <114702513+fire8327@users.noreply.github.com>
2022-12-12 09:55:05 +0000

    create header

---
+++ Assets/JS/custom.js
@@ -0,0 +1,33 @@
+/* navigation */
+/*jshint esnext: true */
+const toggler = document.getElementById("toggler");
+const mNav = document.getElementById("mobile__nav");
+const close = document.getElementById("toggler__expanded");
+const overlay = document.getElementById("overlay");
+const body = document.getElementsByTagName("body")[0];
+
+toggler.addEventListener("click", () => {
+  mNav.classList.remove("translate-x-full");
+  overlay.classList.remove("hidden");
+  body.classList.add("overflow-hidden");
+});
+
+close.addEventListener("click", () => {
+  mNav.classList.add("translate-x-full");
+  overlay.classList.add("hidden");
+  body.classList.remove("overflow-hidden"); 
+});
+
+overlay.addEventListener("click", () => {
+  mNav.classList.add("translate-x-full");
+  overlay.classList.add("hidden"); 
+  body.classList.remove("overflow-hidden"); 
+});
+
+/* preloader */
+const preloader = document.getElementById("preloader");
+window.onload = ()=> {
+  body.classList.remove("overflow-hidden");
+  preloader.classList.add("opacity-0");
+  setTimeout(()=>preloader.classList.add("hidden"), 800);
+};
--- Assets/JS/tailwind.config.js
+++ Assets/JS/tailwind.config.js
@@ -1,8 +1,17 @@
 tailwind.config = {
   theme: {
     extend: {
-      colors: {
+      colors: {            
+          'text': '#030A1B',
         },
-      },
+    },
+    screens: {
+      'ml':'425px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
 };
